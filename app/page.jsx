10d3064a3c3dfdc51f24b26da7e124a1a4d59928
Daniel S <daniels.training97@gmail.com>
2024-04-09 22:08:12 +0100

    fixed some formatting errors

--- app/page.jsx
+++ app/page.jsx
@@ -9,7 +9,7 @@ const Home = () => {
     <div>
         <Nav/>
         <section>
-            <div className='flex justify-between px-36'>
+            <div className='flex justify-between px-36 py-10 bg-green-200'>
                 <div className='w-1/2'>
                    <h1 className='text-7xl font-palanquin my-10'>Become a seller on Pail</h1>
                     <p className='text-3xl font-palanquin my-10'>Reach more customers than ever before</p>
@@ -31,19 +31,21 @@ const Home = () => {
             </div>
         </section>
         <section>
-            <h1 className='text-7xl font-palanquin my-10'>Sell more with Pail</h1>
-            <div className='flex justify-between align-middle'>
-                <div>
-                    <h2>Reach Millions</h2>
-                    <p>Enjoy the reach of our platform, bringing your products to a larger market.</p>
-                </div>
-                <div>
-                    <h2>Streamline your Operation</h2>
-                    <p>Our platform groups buyers into large scale orders, so you can streamline your operation.</p>
-                </div>
-                <div>
-                    <h2>Enhance Brand Exposure</h2>
-                    <p>Joining our platform grants you exposure to your target audience, offering a unique opportunity to showcase your products and expand your brand presence.</p>
+            <div className='text-white bg-black px-36 py-10'>
+                <h1 className='text-7xl font-palanquin w-100 bg-red-200 text-center'>Sell more with Pail</h1>
+                <div className='flex justify-between align-middle'>
+                    <div>
+                        <h2>Reach Millions</h2>
+                        <p>Enjoy the reach of our platform, bringing your products to a larger market.</p>
+                    </div>
+                    <div>
+                        <h2>Streamline your Operation</h2>
+                        <p>Our platform groups buyers into large scale orders, so you can streamline your operation.</p>
+                    </div>
+                    <div>
+                        <h2>Enhance Brand Exposure</h2>
+                        <p>Joining our platform grants you exposure to your target audience, offering a unique opportunity to showcase your products and expand your brand presence.</p>
+                    </div>
                 </div>
             </div>
         </section>
