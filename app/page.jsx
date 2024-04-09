ef5ec6299ac18b8481f3a6617f074cef6bc6e140
Daniel S <daniels.training97@gmail.com>
2024-04-09 22:46:21 +0100

    added final homepage section

--- app/page.jsx
+++ app/page.jsx
@@ -9,10 +9,10 @@ const Home = () => {
     <div>
         <Nav/>
         <section>
-            <div className='flex justify-between px-36 py-10 bg-green-200'>
+            <div className='flex justify-between px-36 pb-20 pt-10'>
                 <div className='w-1/2'>
                    <h1 className='text-7xl font-palanquin my-10'>Become a seller on Pail</h1>
-                    <p className='text-3xl font-palanquin my-10'>Reach more customers than ever before</p>
+                    <p className='text-2xl font-palanquin my-10'>Reach more customers than ever before</p>
                     <button
                         type="button"
                         onClick = {() => {}}
@@ -31,21 +31,42 @@ const Home = () => {
             </div>
         </section>
         <section>
-            <div className='text-white bg-black px-36 py-10'>
-                <h1 className='text-7xl font-palanquin w-100 bg-red-200 text-center'>Sell more with Pail</h1>
-                <div className='flex justify-between align-middle'>
+            <div className='text-white bg-black px-36 py-20'>
+                <h1 className='text-7xl font-palanquin w-100 text-center'>Sell More with Pail</h1>
+                <div className='grid grid-cols-3 gap-10 my-10'>
                     <div>
-                        <h2>Reach Millions</h2>
-                        <p>Enjoy the reach of our platform, bringing your products to a larger market.</p>
+                        <h2 className='text-5xl font-palanquin my-5'>Reach Millions</h2>
+                        <p className='text-2xl font-palanquin'>Enjoy the reach of our platform, bringing your products to a larger market.</p>
                     </div>
                     <div>
-                        <h2>Streamline your Operation</h2>
-                        <p>Our platform groups buyers into large scale orders, so you can streamline your operation.</p>
+                        <h2 className='text-5xl font-palanquin my-5'>Streamline your Operation</h2>
+                        <p className='text-2xl font-palanquin'>Our platform groups buyers into large scale orders, so you can streamline your operation.</p>
                     </div>
                     <div>
-                        <h2>Enhance Brand Exposure</h2>
-                        <p>Joining our platform grants you exposure to your target audience, offering a unique opportunity to showcase your products and expand your brand presence.</p>
+                        <h2 className='text-5xl font-palanquin my-5'>Enhance Brand Exposure</h2>
+                        <p className='text-2xl font-palanquin'>Joining our platform grants you exposure to your target audience, offering a unique opportunity to showcase your products and expand your brand presence.</p>
                     </div>
+                </div>
+            </div>
+        </section>
+        <section>
+            <div className='px-36 py-20'>
+                <h1 className='text-7xl font-palanquin w-100 text-center'>Sign Up Today!</h1>
+                <div className='relative'>
+                    <div className='w-full h-full flex justify-center align-middle'>
+                        <button
+                            type="button"
+                            onClick = {() => {}}
+                            className="black_btn my-20">
+                            Sign Up
+                        </button> 
+                    </div>
+                    <Image 
+                        src="/assets/images/splash2.svg" 
+                        alt="Splash graphic" 
+                        width={0}
+                        height={0}
+                        className="absolute bottom-0 left-0 right-0 ml-auto mr-auto w-[800px]"/>
                 </div>
             </div>
         </section>
