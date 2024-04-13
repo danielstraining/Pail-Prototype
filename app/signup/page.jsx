4891335b8d83c74f689179fddd49ad375b1baf9c
Daniel S <daniels.training97@gmail.com>
2024-04-13 23:27:04 +0100

    Set up form to be styled

--- app/signup/page.jsx
+++ app/signup/page.jsx
@@ -7,7 +7,6 @@ import { useState, useEffect } from "react";
 function SignUp(){
     return (
         <>
-            <div>Sign Up Page</div>
             <div className="h-screen w-screen flex justify-center items-center">
                 <div className="flex flex-col items-center">
                     <Image 
@@ -19,30 +18,30 @@ function SignUp(){
                     />
                     <div className="sm:shadow-xl px-8 pb-8 pt-12 sm:bg-blue-50 rounded-xl space-y-12 w-[500px]">
                         <h1 className="text-2xl font-palanquin text-center">Create Account</h1>
-                        <form>
+                        <form
+                            onSubmit={() => {}}
+                        >
                             <div>
                                 <h2>Email</h2>
-                                <input name="email"></input>   
+                                <input required name="email"></input>   
                             </div>
                             <div>
                                 <h2>Password</h2>
-                                <input name="password"></input> 
+                                <input required name="password"></input> 
                             </div>
                             <div>
                                 <h2>Confirm Password</h2>
-                                <input name="confirmPassword"></input> 
+                                <input required name="confirmPassword"></input> 
                             </div>
+                            <button
+                                type="submit"
+                                className="black_btn my-10">
+                                Register
+                            </button> 
                         </form> 
-                        <button
-                            type="button"
-                            onClick = {() => {}}
-                            className="black_btn my-10">
-                            Register
-                        </button> 
                         <p>Already have an account? <Link href="/" className="">Sign In</Link></p>
                     </div>
                 </div>
-                
             </div>
         </>
     )
