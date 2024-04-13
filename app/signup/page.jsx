ec1286c41b4942ff4092f0a25038d69265a49958
Daniel S <daniels.training97@gmail.com>
2024-04-13 14:22:27 +0100

    began setting up the form to submit signup data

--- app/signup/page.jsx
+++ app/signup/page.jsx
@@ -1,15 +1,18 @@
 "use client"
-
 import { useState, useEffect } from "react";
 
 function SignUp(){
     return (
         <>
             <div>Sign Up Page</div>
-            <form>
-
-            </form>
-        
+            <div className="h-screen w-screen flex justify-center items-center">
+                <div className="sm:shadow-xl px-8 pb-8 pt-12 sm:bg-white rounded-xl space-y-12 w-[500px]">
+                   <form>
+                    <input name="email"></input>
+                    <input name="password"></input>
+                    </form> 
+                </div>
+            </div>
         </>
     )
 }
