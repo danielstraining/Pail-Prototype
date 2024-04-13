30ede7d006829f5e6c587d91d8f39bab97d35607
Daniel S <daniels.training97@gmail.com>
2024-04-13 14:45:20 +0100

    register form update

--- app/signup/page.jsx
+++ app/signup/page.jsx
@@ -1,4 +1,6 @@
 "use client"
+
+import Image from "next/image";
 import { useState, useEffect } from "react";
 
 function SignUp(){
@@ -7,9 +9,17 @@ function SignUp(){
             <div>Sign Up Page</div>
             <div className="h-screen w-screen flex justify-center items-center">
                 <div className="sm:shadow-xl px-8 pb-8 pt-12 sm:bg-white rounded-xl space-y-12 w-[500px]">
-                   <form>
-                    <input name="email"></input>
-                    <input name="password"></input>
+                    <Image 
+                        src="/assets/images/logo.svg" 
+                        alt="Pail Logo" 
+                        width={100}
+                        height={100}
+                        className="object-contain"
+                    />
+                    <h1 className="text-2xl font-palanquin text-center">Create Account</h1>
+                    <form>
+                        <input name="email"></input>
+                        <input name="password"></input>
                     </form> 
                 </div>
             </div>
