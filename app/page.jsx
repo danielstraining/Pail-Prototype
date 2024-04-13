9b011858bbe1ba39593bde96964b5e6397b0ab5a
Daniel S <daniels.training97@gmail.com>
2024-04-13 22:18:16 +0100

    set up the supplier schema

--- app/page.jsx
+++ app/page.jsx
@@ -4,7 +4,6 @@ import '@styles/globals.css'
 import Nav from '@components/Nav'
 import Image from "next/image";
 import { useRouter } from "next/navigation"
-import { connectToDB } from '@utils/database';
 
 const Home = () => {
     const router = useRouter();
