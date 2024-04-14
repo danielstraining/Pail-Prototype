1d33f51d93c7d1391c8be9e414d030efcd71f407
Daniel S <daniels.training97@gmail.com>
2024-04-14 10:26:06 +0100

    Added more styling to register form. Could still be improved but good for now

--- app/signup/page.jsx
+++ app/signup/page.jsx
@@ -14,32 +14,33 @@ function SignUp(){
                         alt="Pail Logo" 
                         width={100}
                         height={100}
-                        className="object-contain"
+                        className="object-contain mb-5"
                     />
-                    <div className="sm:shadow-xl px-8 pb-8 pt-12 sm:bg-blue-50 rounded-xl space-y-12 w-[500px]">
-                        <h1 className="text-2xl font-palanquin text-center">Create Account</h1>
-                        <form
+                    <div className="shadow-xl p-8 bg-blue-50 rounded-xl w-[450px] font-palanquin">
+                        <h1 className="text-3xl text-center mt-1 mb-3">Create Account</h1>
+                        <form 
+                            className="!m-0 p-0"
                             onSubmit={() => {}}
                         >
-                            <div>
-                                <h2>Email</h2>
-                                <input required name="email"></input>   
+                            <div className="my-4">
+                                <h2 className="input_header">Email</h2>
+                                <input className="form_input" required name="email"></input>   
                             </div>
-                            <div>
-                                <h2>Password</h2>
-                                <input required name="password"></input> 
+                            <div className="my-4">
+                                <h2 className="input_header">Password</h2>
+                                <input className="form_input" required name="password"></input> 
                             </div>
-                            <div>
-                                <h2>Confirm Password</h2>
-                                <input required name="confirmPassword"></input> 
+                            <div className="my-4">
+                                <h2 className="input_header">Confirm Password</h2>
+                                <input className="form_input" required name="confirmPassword"></input> 
                             </div>
                             <button
                                 type="submit"
-                                className="black_btn my-10">
+                                className="black_btn my-6 w-full">
                                 Register
                             </button> 
                         </form> 
-                        <p>Already have an account? <Link href="/" className="">Sign In</Link></p>
+                        <p className="w-full text-center !m-0" type="hidden">Already have an account? <Link href="/" className=" text-blue-700">Sign In</Link></p>
                     </div>
                 </div>
             </div>
