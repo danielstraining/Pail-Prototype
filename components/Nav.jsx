a9aa75971680cf78198b5c788bb533bda22c2b2e
Daniel S <daniels.training97@gmail.com>
2024-04-14 10:33:01 +0100

    hooked up the signup button in the navbar

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -2,36 +2,43 @@
 
 import Link from "next/link";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 const Nav = () => {
-  return (
-    <nav className="flex justify-between items-center w-full px-28 py-5">
-        <div>
-            <Link href="/" className="">
-                <Image 
-                    src="/assets/images/logo.svg" 
-                    alt="Pail Logo" 
-                    width={125}
-                    height={125}
-                    className="object-contain"/>
-            </Link>  
-        </div>
-        <div className="flex justify-between w-96">
-            <button
-                type="button"
-                onClick = {() => {}}
-                className="black_btn">
-                Sign Up
-            </button>
-            <button
-                type="button"
-                onClick = {() => {}}
-                className="black_btn">
-                Sign In
-            </button>
-        </div>
-    </nav>
-  )
+    const router = useRouter();
+
+    const handleSignUp = () => {
+        router.push("/signup");
+    }
+
+    return (
+        <nav className="flex justify-between items-center w-full px-28 py-5">
+            <div>
+                <Link href="/" className="">
+                    <Image 
+                        src="/assets/images/logo.svg" 
+                        alt="Pail Logo" 
+                        width={100}
+                        height={100}
+                        className="object-contain"/>
+                </Link>  
+            </div>
+            <div className="flex justify-between w-96">
+                <button
+                    type="button"
+                    onClick = {handleSignUp}
+                    className="black_btn">
+                    Sign Up
+                </button>
+                <button
+                    type="button"
+                    onClick = {() => {}}
+                    className="black_btn">
+                    Sign In
+                </button>
+            </div>
+        </nav>
+    )
 }
 
 export default Nav;
