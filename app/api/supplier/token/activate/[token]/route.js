8351a65c09aa60e90223ce353236d73b7e0ec75f
Daniel S <daniels.training97@gmail.com>
2024-04-22 21:16:08 +0100

    implemented activate account logic

--- app/api/supplier/token/activate/[token]/route.js
+++ app/api/supplier/token/activate/[token]/route.js
@@ -1,26 +1,37 @@
 import { connectToDB } from "@utils/database";
 import ActivationToken from "@models/activationToken";
+import Supplier from "@models/supplier";
 import { hoursElapsed } from "@utils/utils";
+import { redirect } from "next/navigation";
 
 export const GET = async (request, { params }) => {
     try{
-        console.log("ACTIVATING TOKEN ENDPOINT")
+        
         // Connect to the database
         await connectToDB();
 
-        console.log("Params: ", params)
-
         // fetch an array containing all entries with the requested email
-        const token = await ActivationToken.find({
+        const token = await ActivationToken.findOne({
             token: params.token
         })
 
-        const tokenAge = await hoursElapsed(Date.parse(token[0].createdAt))
+        const tokenAge = await hoursElapsed(Date.parse(token.createdAt))
 
-        if (token) {
-            console.log(`Token found! Hours since creation: ${tokenAge}`)
+        if (!token) {
+            // Token does not exist error
+        } else if (tokenAge > 24) {
+            // Token older than 24 hours error
+        } else {
+            const account = await Supplier.findOne({
+                email: token.email
+            })
+
+            // Change active to true and save
+            account.active = true;
+            await account.save();
+            console.log("Account activated successfully");
         }
-    
+
         return new Response({status: 200})
     } catch (error) {
         console.log("Failed to activate token")
