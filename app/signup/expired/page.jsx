270f191b24bc132bd642143ea11ea61ae047fc77
Daniel S <daniels.training97@gmail.com>
2024-04-25 21:46:16 +0100

    changed expired token text

--- app/signup/expired/page.jsx
+++ app/signup/expired/page.jsx
@@ -2,7 +2,7 @@
 
 const Expired = () => {
   return (
-    <div>This token is expired. Please request a new token.</div>
+    <div>This token is expired. Please request a new token from the email.</div>
   )
 }
 
