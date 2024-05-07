f1117f354035958aead26a2de7fbd6305eaebf63
Daniel S <daniels.training97@gmail.com>
2024-05-07 16:40:28 +0100

    redirect back to login page if not signed in

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -7,7 +7,7 @@ import { authOptions } from "@app/api/auth/[...nextauth]/route";
 export default async function Dashboard() {
   
   const session = await getServerSession(authOptions)
-  if (!session) redirect("/")
+  if (!session) redirect("/signin")
 
   return <UserInfo />;
 }
