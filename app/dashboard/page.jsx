a172c045e627a92536dd82e895ddf5c7ba27859b
Daniel S <daniels.training97@gmail.com>
2024-05-07 15:27:20 +0100

    Added protection for the dashboard so it can only be accessed if logged in

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,5 +1,13 @@
 import UserInfo from "@/components/UserInfo";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { authOptions } from "@app/api/auth/[...nextauth]/route";
 
-export default function Dashboard() {
+
+export default async function Dashboard() {
+  
+  const session = await getServerSession(authOptions)
+  if (!session) redirect("/")
+
   return <UserInfo />;
 }
