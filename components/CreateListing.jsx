1cd06a56082691ef2d63cf2a5f9b5fc14b7e29ad
Daniel S <daniels.training97@gmail.com>
2024-05-19 22:29:41 +0100

    Modularised a lot of the design for the create listing page. introduced a 2 track page format

--- components/CreateListing.jsx
+++ components/CreateListing.jsx
@@ -1,80 +1,90 @@
 import Image from "next/image"
 
 
-const CreateListingHeader = ({ images = [] }) => {
+const CreateListingTitle = () => {
     return (
-        <section className="my-16">
-            <div className="w-full flex justify-between">
-                <div className="w-2/3">
-                    <h3 className="text-3xl"></h3>
-                </div>
-                <div className="relative flex justify-end items-end w-1/3">
-                    <Image
-                        src="/assets/images/ColouredSplash1.svg"
-                        alt="Splash graphic"
-                        width={0}
-                        height={0}
-                        className="absolute w-5/6 right-0 bottom-0" />
-                </div>
-            </div>
+        <div className="w-full bg-pail_tan rounded-xl p-5">
+            <h1 className="text-2xl mb-5">Product Title</h1>
+            <input onChange={() => { }} className="form_input w-full" required name="Title" />
+        </div>
+    )
+}
 
-            <div className="w-full flex justify-between items-center my-10 h-[450px]">
-                <div className="flex flex-col h-full w-1/2 mr-5">
-                    <CreateListingImages images={images}/>
-                </div>
-                <div className="h-full flex flex-col w-1/2 ml-5">
-                    <div className="w-full mb-5 p-5">
-                        <h1 className="text-2xl mb-5">Product Title</h1>
-                        <input onChange={() => { }} className="form_input" required name="Title" />
-                    </div>
-                    <div className="flex flex-col w-full mt-5 p-5 h-full">
-                        <h1 className="text-2xl mb-5">Product Description</h1>
-                        <textarea onChange={() => { }} className="form_input h-full resize-none" required name="Description" />
-                    </div>
-                </div>
-            </div>
-        </section>
+const CreateListingDescription = () => {
+    return (
+        <div className="w-full bg-pail_tan rounded-xl p-5">
+            <h1 className="text-2xl mb-5">Product Description</h1>
+            <textarea onChange={() => { }} className="form_input w-full min-h-80 resize-none" required name="Description" />
+        </div>
     )
 }
 
 const CreateListingVariations = () => {
     return (
-        <section className="my-16">
-            <h2 className="text-3xl my-10">Variations</h2>
-            <div className="w-full h-80 bg-pail_tan rounded-xl my-10"></div>
+        <section>
+            <div className="bg-pail_tan flex flex-col items-center rounded-xl w-full p-5">
+                <Image
+                    src="/assets/icons/Shapes.svg"
+                    alt="Splash graphic"
+                    width={0}
+                    height={0}
+                    className="w-1/2 max-w-52 my-3" />
+                <button
+                    type="button"
+                    onClick={() => { }}
+                    className="black_btn bg-pail_green border-pail_green cursor-pointer my-5">
+                    Add variations
+                </button>
+                <p className="text-center px-5 my-3 text-xl">(Size, colour, flavour...)</p>
+            </div>
         </section>
     )
 }
 
 const CreateListingDynamicPricing = () => {
     return (
-        <section className="my-16">
-            <h2 className="text-3xl my-10">Dynamic Pricing</h2>
-            <div className="w-full h-80 bg-pail_tan rounded-xl my-10"></div>
+        <section>
+            <div className="w-full h-80 bg-pail_tan rounded-xl p-5">
+                <h2 className="text-2xl mb-5">Dynamic Pricing</h2>
+                <div className="text-xl mb-5">
+                    <p className="my-5"> 
+                        The minimum order quantity is
+                        <input onChange={() => { }} className="form_input w-20 mx-2" required name="MinOrderQuantity"/>
+                        units costing $
+                        <input onChange={() => { }} className="form_input w-20 mx-2" required name="MaxUnitPrice"/>
+                        per unit.
+                    </p>
+                    <p className="my-5"> 
+                        The lowest unit price is $
+                        <input onChange={() => { }} className="form_input w-20 mx-2" required name="MinOrderQuantity"/>
+                        per unit occuring at
+                        <input onChange={() => { }} className="form_input w-20 mx-2" required name="MaxUnitPrice"/>
+                        units.
+                    </p>
+                </div>
+            </div>
         </section>
     )
 }
 
-const CreateListingImages = ({ images }) => {
+const CreateListingImages = ({ images = [] }) => {
     return (
         <>
             {images.length <= 0 ?
-                <div className="bg-pail_tan flex flex-col rounded-xl w-full h-full p-16">
-                    <div className="w-full h-full flex flex-col justify-between items-center">
-                        <Image
-                            src="/assets/icons/ImageIcon.svg"
-                            alt="Splash graphic"
-                            width={0}
-                            height={0}
-                            className="w-44" />
-                        <button
-                            type="button"
-                            onClick={() => { }}
-                            className="black_btn bg-pail_red border-pail_navy cursor-pointer">
-                            Add images
-                        </button>
-                        <p className="text-center px-5">(Images must be in png or jpg format and be between xgb and ygb)</p>
-                    </div>
+                <div className="bg-pail_tan flex flex-col items-center rounded-xl w-full p-5">
+                    <Image
+                        src="/assets/icons/ImageIcon.svg"
+                        alt="Splash graphic"
+                        width={0}
+                        height={0}
+                        className="w-1/2 max-w-52 my-3" />
+                    <button
+                        type="button"
+                        onClick={() => { }}
+                        className="black_btn bg-pail_red border-pail_red cursor-pointer my-5">
+                        Add images
+                    </button>
+                    <p className="text-center px-5 my-3 text-xl">(Images must be in png or jpg format and be between xgb and ygb)</p>
                 </div>
                 :
                 <div>There are some images to display</div>
@@ -87,11 +97,23 @@ const CreateListingImages = ({ images }) => {
 
 export const CreateListing = () => {
     return (
-        <>
-            <CreateListingHeader />
-            <CreateListingVariations />
-            <CreateListingDynamicPricing />
-        </>
+        <section className="flex gap-5">
+            <div className="w-3/5 flex flex-col gap-5">
+                <CreateListingImages />
+                <CreateListingDynamicPricing />
+            </div>
+            <div className="w-2/5 flex flex-col gap-5">
+                <CreateListingTitle />
+                <CreateListingDescription />
+                <CreateListingVariations />
+                <button
+                    type="button"
+                    onClick={() => { }}
+                    className="black_btn bg-pail_blue border-pail_blue mx-auto cursor-pointer my-5 w-full">
+                    Preview listing
+                </button>
+            </div>
+        </section>
 
 
     )
